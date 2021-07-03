2a0bc5d1ec11ec1c284c4da1553f080e6790b96d
Vitalii Kulbachenko <kulbachenko.v@ikajo.com>
2021-07-03 12:50:43 +0300

    update

--- build.sh
+++ build.sh
@@ -64,7 +64,7 @@ read DISTR
   case $DISTR in
     build-1|1)            docker build -t $name/$vendor-mysql57 ./mysql ;;
     build-2|2)            docker build -t $name/$vendor-nginx ./nginx ;;
-    build-3|3)            docker build -t $name/$vendor-php-fpm ./php-fpm ;;
+    build-3|3)            docker build -t --no-cache $name/$vendor-php-fpm ./php-fpm ;;
 
 
      *)
