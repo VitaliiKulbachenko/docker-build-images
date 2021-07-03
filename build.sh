2a6d12b6afeacfee932ae90167d3178dde0dd33e
Vitalii Kulbachenko <kulbachenko.v@ikajo.com>
2021-07-03 13:01:50 +0300

    update

--- build.sh
+++ build.sh
@@ -54,7 +54,7 @@ ECHO_QUESTION -n \
 ***********************************************************
 * 1  - Build :=> Mysql57
 * 2  - Build :=> Nginx
-* 3  - Build :=> Php-fpm 7.3
+* 3  - Build :=> Php-fpm 7.4
 ***********************************************************
 " \
 "Choose installation option: => "
@@ -64,7 +64,7 @@ read DISTR
   case $DISTR in
     build-1|1)            docker build -t $name/$vendor-mysql57 ./mysql ;;
     build-2|2)            docker build -t $name/$vendor-nginx ./nginx ;;
-    build-3|3)            docker build -t --no-cache $name/$vendor-php-fpm ./php-fpm ;;
+    build-3|3)            docker build --no-cache --build-arg PHP_VERSION=74 -t $name/$vendor-php-fpm ./php-fpm ;;
 
 
      *)
